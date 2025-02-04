dfbfcd8ed160d509744d1152c83ab1394608d6be
Bryan Marshall <bryan.marshall@gcsu.edu>
2025-02-04 10:48:02 -0500

    asdf

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,15 @@
 from flask import render_template
+from datetime import datetime
 from . import app
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', year=datetime.now().year)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', year=datetime.now().year)
 
 @app.route('/quarterbacks')
 def quarterbacks():
-    return render_template('quarterbacks.html')
+    return render_template('quarterbacks.html', year=datetime.now().year)
